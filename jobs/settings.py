087d25580fb5a45effcad93ee8a2d8c589d29d53
Raj Sahu <80415025+RSahu20@users.noreply.github.com>
2024-01-12 23:42:39 +0530

    Update settings.py

--- jobs/settings.py
+++ jobs/settings.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-from pathlib import Path
-import dj_database_url
+
 import os
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -76,33 +75,29 @@ WSGI_APPLICATION = 'jobs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
-    
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-     },
-     {
-         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-     },
-     {
-         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-     },
-    {
-         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-     },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 # Internationalization
